23c3f2987f19922583133681a4cb79541b04a608
Felipe Trosi <felipetrosi123@gmail.com>
2025-12-28 13:24:23 -0300

    Alterações gerais

---
+++ src/FIAP.FCG.Transaction.Infrastructure/Migrations/20251228162010_InitialCommit.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace FIAP.FCG.Transaction.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCommit : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Transaction",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Code = table.Column<long>(type: "bigint", nullable: false),
+                    UserId = table.Column<long>(type: "bigint", nullable: false),
+                    GameId = table.Column<long>(type: "bigint", nullable: false),
+                    Type = table.Column<string>(type: "varchar(30)", nullable: false),
+                    Status = table.Column<string>(type: "varchar(30)", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "timestamp without time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Transaction", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Transaction");
+        }
+    }
+}
--- src/FIAP.FCG.Transaction.Infrastructure/Repository/ApplicationDbContext.cs
+++ src/FIAP.FCG.Transaction.Infrastructure/Repository/ApplicationDbContext.cs
@@ -1,44 +1,47 @@
 ﻿using FIAP.FCG.Transaction.Domain.Entity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
 using Microsoft.Extensions.Configuration;
 
 namespace FIAP.FCG.Transaction.Infrastructure.Repository
 {
     public class ApplicationDbContext : DbContext
     {
-        private readonly string _connectionString = null!; 
-
-
-        public ApplicationDbContext()
-        {
-            IConfiguration cfg = new ConfigurationBuilder()
-                .SetBasePath(AppDomain.CurrentDomain.BaseDirectory)
-                .AddJsonFile("appsettings.json")
-                .Build();
-
-            _connectionString = cfg.GetConnectionString("DefaultConnection")!;
-        }
-
-        public ApplicationDbContext(string connectionString)
-        {
-            _connectionString = connectionString;
-        }
-
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options) { }
 
         public DbSet<TransactionEntity> Transaction { get; set; }
-
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        {
-            if (!optionsBuilder.IsConfigured)
-            {
-                optionsBuilder.UseNpgsql(_connectionString);
-            }
-        }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly);
         }
+    }
 
+    public class ApplicationDbContextFactory : IDesignTimeDbContextFactory<ApplicationDbContext>
+    {
+        public ApplicationDbContext CreateDbContext(string[] args)
+        {
+            var basePath = Path.Combine(
+                Directory.GetCurrentDirectory(),
+                "..",
+                "FIAP.FCG.Transaction.API"
+            );
+
+            IConfiguration configuration = new ConfigurationBuilder()
+                .SetBasePath(basePath)
+                .AddJsonFile("appsettings.json", optional: false)
+                .AddJsonFile("appsettings.Development.json", optional: true)
+                .Build();
+
+            var connectionString = configuration.GetConnectionString("DefaultConnection");
+
+
+             var options = new DbContextOptionsBuilder<ApplicationDbContext>()
+                .UseNpgsql(connectionString)
+                .Options;
+
+            return new ApplicationDbContext(options);
+        }
     }
 }
